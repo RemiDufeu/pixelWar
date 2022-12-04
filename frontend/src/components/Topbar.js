06446c2559cc3731bd6ce4ad9b912d2086503950
fatmadridi <dridifatma21@gmail.com>
2022-12-04 15:41:28 +0100

    Suppression du toggle en mode déconnecté

--- frontend/src/components/Topbar.js
+++ frontend/src/components/Topbar.js
@@ -145,18 +145,6 @@ const TopBar = () => {
                 </Navbar>
                 <br/>
                 <br/>
-                <div className="container--toggle">
-                    {
-                        toggle === "light" ?
-                            <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick}
-                                   checked/>
-                            :
-                            <input type="checkbox" id="toggle" className="toggle--checkbox" onClick={handleOnClick}/>
-                    }
-                    <label htmlFor="toggle" className="toggle--label">
-                        <span className="toggle--label-background"></span>
-                    </label>
-                </div>
             </>
         )
     }
